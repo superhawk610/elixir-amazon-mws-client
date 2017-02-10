cc54838760bc9ce70a00ff9d7fc6eefecc356aca
Roman Sotnikov <roman.sotnikov@gmail.com>
2017-02-10 18:19:33 +0700

    Add deregister_destination

--- test/mws_client/subscriptions_test.exs
+++ test/mws_client/subscriptions_test.exs
@@ -15,4 +15,10 @@ defmodule MWSClient.SubscriptionsTest do
 
     assert res == exp
   end
+
+  test "deregister_destination should return a struct" do
+    res = Subscriptions.deregister_destination("http://foo.bar/baz")
+    exp = %MWSClient.Operation{body: nil, headers: [], method: "POST", path: "/Subscriptions/2013-07-01", timestamp: nil, params: %{"Destination.AttributeList.member.1.Key" => "sqsQueueUrl", "Destination.AttributeList.member.1.Value" => "http://foo.bar/baz", "Destination.DeliveryChannel" => "SQS", "MarketplaceId" => "ATVPDKIKX0DER", "Version" => "2013-07-01", "Action" => "DeregisterDestination"}}
+    assert res == exp
+  end
 end
